1171ab8182753300c266901588e9983aaac3853a
VoltedSwitch <wirelessvoltswitch@gmail.com>
2025-08-06 13:38:11 +0500

    reformat and bold

--- static/js/utils.js
+++ static/js/utils.js
@@ -49,7 +49,8 @@ function afterUserWon() {
   const messageArea = document.getElementById(CLASSNAMES.result);
 
   const p = document.createElement("p");
-  p.innerHTML = "<strong>🎉 You clicked all correct buttons! Pattern complete!</strong>";
+  p.innerHTML =
+    "<strong>🎉 You clicked all correct buttons! Pattern complete!</strong>";
 
   const againButton = document.createElement("button");
   againButton.textContent = "Puzzle Again?";
@@ -69,10 +70,13 @@ function afterUserLost(correctSequence) {
   const messageArea = document.getElementById(CLASSNAMES.result);
   // "❌ Wrong button. You lost."
   const p1 = document.createElement("p");
-  p1.innerHTML = "<strong>❌ You clicked on the wrong button! Wrong pattern!</strong>";
+  p1.innerHTML =
+    "<strong>❌ You clicked on the wrong button! Wrong pattern!</strong>";
 
   const p2 = document.createElement("p");
-  p2.textContent = `Correct Pattern: ${correctSequence.join(" --> ")}`;
+  p2.innerHTML = `<strong>Correct Pattern:</strong> ${correctSequence.join(
+    " --> "
+  )}`;
 
   const tryAgainButton = document.createElement("button");
   tryAgainButton.textContent = "Try Again?";
